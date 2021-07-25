dd162fe0ad8423a0da6c27ed0490a42291daa320
KazumaNakaDev <84255940+KazumaNakaDev@users.noreply.github.com>
2021-07-25 17:10:02 -0400

    test

--- src/test/java/FirefoxBrowserTest.java
+++ src/test/java/FirefoxBrowserTest.java
@@ -1,17 +1,28 @@
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.*;
 
 public class FirefoxBrowserTest {
 
-    @Test
-    public void testMain(){
+    private FirefoxDriver driver;
+
+    @Before
+    public void setUp() {
         System.setProperty("webdriver.gecko.driver", "./src/main/resources/drivers/geckodriver");
-        FirefoxDriver driver = new FirefoxDriver();
-        driver.get("https://www.google.com");
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void OpenGoogleOnFirefox(){
+        driver.get("https://www.google.com/");
+    }
+
+    @After
+    public void tearDown() {
         driver.close();
     }
+
 }
